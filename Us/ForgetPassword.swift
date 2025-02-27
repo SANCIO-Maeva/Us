5516dcf9285c138c1caa81c36a09015f6febed7b
Maeva SANCIO <maevasancio@lesperserverants.fr>
2025-02-27 23:00:48 +0100

    add first commit

---
+++ Us/ForgetPassword.swift
@@ -0,0 +1,18 @@
+//
+//  ForgetPassword.swift
+//  Us
+//
+//  Created by Maëva SANCIO on 27/02/2025.
+//
+
+import SwiftUI
+
+struct ForgetPassword: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ForgetPassword()
+}
